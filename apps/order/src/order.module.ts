8368ed2b2e9776096c2f65f1d5b9f6defe07166c
Mohsen Bostan <mohsenbostandev@gmnail.com>
2023-01-18 05:13:20 +0330

    feat(order): add grpc error exception

--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { GrpcServerExceptionFilter } from 'nestjs-grpc-exceptions';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
   imports: [],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [
+    OrderService,
+    {
+      provide: APP_FILTER,
+      useClass: GrpcServerExceptionFilter,
+    },
+  ],
 })
 export class OrderModule {}
--- apps/order/src/order.service.ts
+++ apps/order/src/order.service.ts
@@ -1,9 +1,14 @@
 import { FindOrderByIdRequest, IOrder, IOrderService } from '@app/shared';
 import { Injectable } from '@nestjs/common';
+import { GrpcNotFoundException } from 'nestjs-grpc-exceptions';
 
 @Injectable()
 export class OrderService implements IOrderService {
   findOneById({ id }: FindOrderByIdRequest): IOrder {
+    if (id !== 'order-1') {
+      throw new GrpcNotFoundException('Order not found.');
+    }
+
     return {
       id,
       name: 'Test Product 1',
